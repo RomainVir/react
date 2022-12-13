ad1236907b097a734742c59a4a88decf2b0bc9a7
Romain Viravaud <romainviravaud@gmail.com>
2022-12-13 18:46:29 +0100

    lint

---
+++ my-first-app/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Card  from '../src/components/card/card'
+import '../src/components/card/card'
+
+
+function App() {
+  return (
+  <div className='cards'>
+  <Card>
+  
+  </Card>
+  
+  </div>
+
+  );
+}
+
+export default App;
---
+++ my-first-app/src/components/card/card.js
@@ -0,0 +1,17 @@
+import Img from '../../imagenes/canyon3.jpeg'
+export default function card() {
+  return (
+<div className ="cards">
+<img src={Img}></img>
+      <h3>Grand Canyon 5 WMN</h3>
+      <h4>1459 €</h4>
+      <span>★</span>
+         <span>★</span>
+         <span>★</span>
+         <span>★</span>
+         <span>★</span>
+     
+       </div>
+  );
+}
+
