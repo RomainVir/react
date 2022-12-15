fb32447196f44abd9d481e7f6b80b6752c0fb63c
Romain Viravaud <romainviravaud@gmail.com>
2022-12-15 16:23:07 +0100

    ok

---
+++ proyecto-vite/src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+export function Home({ img, title, price, buy, details }) {
+    return (
+      <div className="cards">
+        <img src={img} alt="velo" />
+        <h3>{title}</h3>
+        <h4>{price}</h4>
+        <button>{buy}</button>
+        <button>{details}</button>
+      </div>
+    );
+  }
+  
