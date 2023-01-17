01e35f7f32e11306a3be155f4de6166ac9272cfa
Romain Viravaud <romainviravaud@gmail.com>
2023-01-17 18:59:43 +0100

    ogin

--- proyecto-vite/src/views/Login/Login.jsx
+++ proyecto-vite/src/views/Login/Login.jsx
@@ -1,63 +1,135 @@
 import "../Login/Login.css";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function LogIn() {
-  const [user, setUser] = useState({
+//pour remettre les inputs a zero quand on s´enregistre
+const initialUserState = {
+  email: "",
+  password: "",
+  name: "",
+  surname: "",
+};
+
+export default function Connexion() {
+  // REGISTER +***************************
+  const [newUser, setNewUser] = useState(initialUserState);
+
+  function handleInputRegister(e) {
+    const newRegister = {
+      ...newUser,
+      [e.target.name]: e.target.value,
+    };
+    setNewUser(newRegister);
+  }
+
+  function registerUser(e) {
+    e.preventDefault();
+    fetch("http://localhost:3000/user", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    }).then((response) => {
+      if (response.status === 401) {
+        throw "Not authorized";
+      } else if (response.status == 200) {
+        setNewUser(initialUserState);
+        alert("User registered!!");
+      } else if (response.status === 409) {
+        alert("User already registered");
+      }
+    });
+  }
+
+  //LOGIN ************
+  const [User, setUser] = useState({
     email: "",
     password: "",
   });
+  const navigate = useNavigate();
 
-  console.log(user);
-  function handleUser(e) {
-    setUser({
-      ...user,
+  function handleLogin(e) {
+    const newLogin = {
+      ...User,
       [e.target.name]: e.target.value,
+    };
+    setUser(newLogin);
+  }
+
+  async function loginUser(e) {
+    e.preventDefault();
+    fetch("http://localhost:3000/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(User),
+    }).then((response) => {
+      if (response.status === 401) {
+        throw "Not authorized";
+      } else if (response.status == 200) {
+        navigate("/");
+      } else {
+        alert("Wrong credentials, try again");
+      }
     });
   }
-  // para simular backend
-  function handleSubmit(e, user) {
-    e.preventDefault();
-    if (user.email === "pepe@mail.com" && user.password === "1234") {
-      alert("Conected");
-    } else {
-      alert("Incorrect!!!");
-    }
-  }
-  //
 
   return (
     <div className="containerconnexion">
       <div className="container2">
-        <div className="register">
+        <form className="register" onSubmit={registerUser}>
           <h1>Register</h1>
 
           <div className="input-grp">
             <label for="Email"></label>
-            <input type="text" placeholder="Name" id="Name" />
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={newUser.name}
+              onChange={handleInputRegister}
+            />
             <div className="input-grp">
               <label for="Email"></label>
-              <input type="text" placeholder="Surname" id="Surname" />
+              <input
+                type="text"
+                name="surname"
+                placeholder="Surname"
+                value={newUser.surname}
+                onChange={handleInputRegister}
+              />
 
               <div className="input-grp">
                 <label for="Email"></label>
-                <input type="email" placeholder="Email" id="Email" />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={newUser.email}
+                  onChange={handleInputRegister}
+                />
               </div>
 
               <div className="input-grp">
-                <label for="Password"></label>
+                <input
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  value={newUser.password}
+                  onChange={handleInputRegister}
+                />
               </div>
-
-              <div className="input-grp">
-                <input type="password" placeholder="Password" id="Password" />
-              </div>
-              <button className="enter " type="button">
+              <button className="enter" type="submit">
                 Register
               </button>
             </div>
           </div>
-        </div>
+        </form>
+
         <div class="login">
-          <form onSubmit={(e) => handleSubmit(e, user)}>
+          <form onSubmit={loginUser}>
             <h1>Log in</h1>
 
             <div class="input-grp">
@@ -65,8 +137,8 @@ export default function LogIn() {
               <input
                 type="email"
                 name="email"
-                value={user.email}
-                onChange={handleUser}
+                value={User.email}
+                onChange={handleLogin}
               />
 
               <div class="input-grp">
@@ -75,8 +147,8 @@ export default function LogIn() {
                   <input
                     type="password"
                     name="password"
-                    value={user.password}
-                    onChange={handleUser}
+                    value={User.password}
+                    onChange={handleLogin}
                   />
                 </div>
               </div>
